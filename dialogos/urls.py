2ad892abcc384c9f8d44da7b94ae6a0d8ec397c4
sg <sg@novel-t.ch>
2023-01-16 14:51:18 +0100

    Replace django.conf.urls with django.urls.path

--- dialogos/urls.py
+++ dialogos/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^comment/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$',
+    path('comment/<int:content_type_id>/<int:object_id>/',
         views.post_comment, name='post_comment'),
-    url(r'^comment/(?P<comment_id>\d+)/delete/$', views.delete_comment,
+    path('comment/<int:comment_id>)/delete/', views.delete_comment,
         name='delete_comment'),
-    url(r'^comment/(?P<comment_id>\d+)/edit/$', views.edit_comment,
+    path('comment/<int:comment_id>/edit/', views.edit_comment,
         name='edit_comment')
 ]
